28fc3884e30d831a7388649e53dbc15af0e8ecfd
Andrew Clemons <andrew.clemons@gmail.com>
2016-05-02 14:59:56 +1200

    Day 8

---
+++ day08/runme.rb
@@ -0,0 +1,24 @@
+def compute_size(input)
+  input.each_line.map do |line|
+    line.chomp!
+
+    line.length - unescape(line).length
+
+  end.reduce(0) { |sum, line| sum + line }
+end
+
+def unescape(line)
+  unquoted = line.slice(1..(line.length - 2))
+  (eval %Q{"#{unquoted}"})
+end
+
+def read_input
+  if ARGV.length > 0
+    File.read(ARGV[0])
+  else
+    STDIN.read
+  end
+end
+
+puts "The number of characters of code for string literals minus the number of characters in memory for the values of the strings is: #{compute_size(read_input)}"
+

91c3a5da745701fc00591817659b3b74e68b52f1
Andrew Clemons <andrew.clemons@gmail.com>
2016-05-02 19:57:13 +1200

    Day 5 - part 2

--- day05/runme.rb
+++ day05/runme.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 def nice?(line)
   return false if line.chars.reduce(0) { |total, character| total + (%w(a e i o u).include?(character) ? 1 : 0) } < 3
   return false unless /(.)\1{1,}/.match(line)
@@ -8,16 +6,29 @@ def nice?(line)
   true
 end
 
-def number_of_nice_strings(input)
-  input.each_line.count {|line| nice?(line)}
+def nice_v2?(line)
+  return false unless /(.).\1{1}/.match(line)
+  return false unless /(..).*\1{1}/.match(line)
+
+  true
+end
+
+def number_of_nice_strings(input, new_rules)
+  input.each_line.count {|line| new_rules ? nice_v2?(line) : nice?(line)}
 end
 
 def read_input
-  if ARGV.length > 0
-    File.read(ARGV[0])
+  if ARGV.length == 2 and ARGV[0]== "--with-new-rules"
+    [File.read(ARGV[1]), true]
+  elsif ARGV.length == 1 and ARGV[0]== "--with-new-rules"
+    [STDIN.read, true]
+  elsif ARGV.length > 0
+    [File.read(ARGV[0]), false]
   else
-    STDIN.read
+    [STDIN.read, false]
   end
 end
 
-puts "Number of nice strings: #{number_of_nice_strings(read_input)}"
+(data, use_new_rules) = read_input
+
+puts "Number of nice strings: #{number_of_nice_strings(data, use_new_rules)}"

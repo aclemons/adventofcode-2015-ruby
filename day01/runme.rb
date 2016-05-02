540946daccbf8749ec1028bcf74577a26bd06b70
Andrew Clemons <andrew.clemons@gmail.com>
2016-05-02 18:50:54 +1200

    Day 01 - part 2

--- day01/runme.rb
+++ day01/runme.rb
@@ -1,6 +1,13 @@
 def calculate_floor(input)
-  input.chars.reduce(0) do |sum, character|
-    case character
+  input.each_char.with_index.reduce([0, nil]) do |tuple, (character,index)|
+
+    sum = tuple[0]
+
+    if sum == -1 and tuple[1].nil?
+      tuple[1] = index
+    end
+
+    tuple[0] = case character
       when '('
         1
       when ')'
@@ -8,6 +15,8 @@ def calculate_floor(input)
       else
         0
     end + sum
+
+    tuple
   end
 end
 
@@ -19,4 +28,7 @@ def read_input
   end
 end
 
-puts "Santa should be on floor: #{calculate_floor(read_input)}"
+(floor, basement_position) = calculate_floor(read_input)
+
+puts "Santa should be on floor: #{floor} and first entered the basement at #{basement_position}"
+

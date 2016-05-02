0c93d98a10c1e29aabfad365ad96090548f2b415
Andrew Clemons <andrew.clemons@gmail.com>
2016-05-02 17:05:24 +1200

    Day 12

---
+++ day12/runme.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+def compute_sum(input)
+  JSON.parse(input).reduce(0) { |sum, (key, value)| sum + sum_json(value) }
+end
+
+def sum_json(node)
+  if node.is_a? Numeric
+    node
+  elsif node.is_a? Array
+    node.map { |val| sum_json(val)}.reduce(0) { |total, val| total + val }
+  elsif node.is_a? Hash
+    node.map { |key, val| sum_json(val) }.reduce(0) { |total, val| total + val }
+  else
+    0
+  end
+end
+
+def read_input
+  if ARGV.length > 0
+    File.read(ARGV[0])
+  else
+    STDIN.read
+  end
+end
+
+puts "The sum of all numbers in the document is: #{compute_sum(read_input)}"
+

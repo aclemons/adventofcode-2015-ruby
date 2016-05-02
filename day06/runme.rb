077c19305335cf66d26488da8c27cee80896404c
Andrew Clemons <andrew.clemons@gmail.com>
2016-05-02 20:16:05 +1200

    day 6 - part 2

--- day06/runme.rb
+++ day06/runme.rb
@@ -1,4 +1,4 @@
-def switch_lights(line, grid)
+def switch_lights(line, grid, new_translation)
   return unless match = /^(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/.match(line)
 
   (action, startx, starty, endx, endy) = match.captures
@@ -8,36 +8,58 @@ def switch_lights(line, grid)
       key = [i, j]
       case action
         when 'turn off'
-          grid.delete key
-        when 'turn on'
-          grid[key] = true
-        when 'toggle'
-          if grid.has_key? key
-            grid.delete key
+          if new_translation
+            grid[key] ||= 0
+            grid[key] -= 1
+            grid.delete(key) if grid[key] <= 0
           else
-            grid[key] = true
+            grid.delete key
+          end
+        when 'turn on'
+          if new_translation
+            grid[key] ||= 0
+            grid[key] += 1
+          else
+            grid[key] = 1
+          end
+        when 'toggle'
+          if new_translation
+            grid[key] ||= 0
+            grid[key] += 2
+          else
+            if grid.has_key? key
+              grid.delete key
+            else
+              grid[key] = 1
+            end
           end
       end
     end
   end
-
 end
 
-def light_count(input)
+
+def light_count(input, new_translation)
   grid = {}
 
-  input.each_line {|line| switch_lights(line, grid)}
+  input.each_line {|line| switch_lights(line, grid, new_translation)}
 
-  grid.size
+  grid.values.reduce(:+)
 end
 
 def read_input
-  if ARGV.length > 0
-    File.read(ARGV[0])
+  if ARGV.length == 2 and ARGV[0]== "--with-new-translation"
+    [File.read(ARGV[1]), true]
+  elsif ARGV.length == 1 and ARGV[0]== "--with-new-translation"
+    [STDIN.read, true]
+  elsif ARGV.length > 0
+    [File.read(ARGV[0]), false]
   else
-    STDIN.read
+    [STDIN.read, false]
   end
 end
 
-puts "The number of burning lights is #{light_count(read_input)}"
+(data, use_new_translation) = read_input
+
+puts "The number of burning lights is #{light_count(data, use_new_translation)}"
 
